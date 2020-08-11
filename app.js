aa405adef9ba40c541b083eef54274d1215d29d5
Brian Levin <bml201095@gmail.com>
2020-08-11 01:53:17 -0400

    function to clear from local storage

--- app.js
+++ app.js
@@ -88,3 +88,8 @@ function clearTasks() {
     // Clear from LS
     clearTasksFromLocalStorage();
   }
+
+  // Clear Tasks from LS
+function clearTasksFromLocalStorage() {
+    localStorage.clear();
+  }
